d83a071718316ac181fb652f3a36e7e24acb875c
Tom Filepp <tom@paperscissorsandglue.com>
2019-05-29 13:08:00 -0700

    move common audio processing functionality into class

---
+++ AudioProcessor.py
@@ -0,0 +1,62 @@
+import uuid
+import os
+import sys
+from pydub import AudioSegment
+
+
+class AudioProcessor:
+    def __init__(self, episode, preroll, postroll=False, amplitude=-20):
+        self.amplitude = amplitude
+        self.filename_uuid = str(uuid.uuid4())
+        self.episode = self.load(episode)
+        self.preroll = self.load(preroll)
+        self.postroll = False
+        if postroll:
+            self.postroll = self.load(postroll)
+
+    def detect_leading_silence(self, sound, silence_threshold=-50.0, chunk_size=10):
+        '''
+        sound is a pydub.AudioSegment
+        silence_threshold in dB
+        chunk_size in ms
+
+        iterate over chunks until you find the first one with sound
+        '''
+        trim_ms = 0 # ms
+
+        assert chunk_size > 0 # to avoid infinite loop
+        while sound[trim_ms:trim_ms+chunk_size].dBFS < silence_threshold and trim_ms < len(sound):
+            trim_ms += chunk_size
+
+        return trim_ms
+
+    def normalize(self, sound):
+        change_in_dBFS = self.amplitude - sound.dBFS
+
+        return sound.apply_gain(change_in_dBFS)
+
+    def load(self, filename):
+        file_prefix, file_extension = os.path.splitext(filename)
+        audio = AudioSegment.from_file(os.path.join(sys.path[0], filename), file_extension.replace('.', ''))
+        return self.normalize(audio)
+
+    def trim_silence(self):
+        start_trim = self.detect_leading_silence(self.episode)
+        end_trim = self.detect_leading_silence(self.episode.reverse())
+        duration = len(self.episode)
+        self.episode = self.episode[start_trim:duration-end_trim]
+        return self
+
+    def export(self, filename_override=False):
+        files = [self.preroll, self.episode]
+
+        if self.postroll:
+            files.append(self.postroll)
+
+        combined = files[0]
+
+        for audio in files[1:]:
+            combined = combined.append(audio, crossfade=50)
+
+        filename = filename_override if filename_override else "episode-"+self.filename_uuid+".mp3"
+        return combined.export(filename, format="mp3", bitrate="256k")
--- main.py
+++ main.py
@@ -1,57 +1,16 @@
-from pydub import AudioSegment
 import time
 import logging
-import uuid
+from AudioProcessor import AudioProcessor
 
-def detect_leading_silence(sound, silence_threshold=-50.0, chunk_size=10):
-    '''
-    sound is a pydub.AudioSegment
-    silence_threshold in dB
-    chunk_size in ms
 
-    iterate over chunks until you find the first one with sound
-    '''
-    trim_ms = 0 # ms
-
-    assert chunk_size > 0 # to avoid infinite loop
-    while sound[trim_ms:trim_ms+chunk_size].dBFS < silence_threshold and trim_ms < len(sound):
-        trim_ms += chunk_size
-
-    return trim_ms
-
-def match_target_amplitude(sound, target_dBFS):
-    change_in_dBFS = target_dBFS - sound.dBFS
-    return sound.apply_gain(change_in_dBFS)
-
-amplitude = -20
-filename = str(uuid.uuid4())
 l = logging.getLogger("pydub.converter")
 l.setLevel(logging.DEBUG)
 l.addHandler(logging.StreamHandler())
 start = time.time()
 
-episode = AudioSegment.from_file("beervana.mp3", "mp3")
+process = AudioProcessor("beervana.mp3", "one.mp3", "two.mp3")
 
-start_trim = detect_leading_silence(episode)
-end_trim = detect_leading_silence(episode.reverse())
-duration = len(episode)
-trimmed_episode = episode[start_trim:duration-end_trim]
-
-trimmed_episode = match_target_amplitude(trimmed_episode, amplitude)
-adspot_1 = AudioSegment.from_file("one.mp3", "mp3")
-adspot_2 = AudioSegment.from_file("two.mp3", "mp3")
-
-adspot_1 = match_target_amplitude(adspot_1, amplitude)
-adspot_2 = match_target_amplitude(adspot_2, amplitude)
-
-files = [adspot_1, trimmed_episode, adspot_2]
-
-combined = files[0]
-
-for audio in files[1:]:
-    combined = combined.append(audio, crossfade=50)
-
-combined.export("episode-"+filename+".mp3", format="mp3", bitrate="256k")
+process.trim_silence().export("somefilename.mp3");
 
 end = time.time()
 print(end - start)
